3f2e42eab048e58d7d8327cb5463b36ad5d45253
BarryZe <98925337+BarryZe@users.noreply.github.com>
2023-01-14 14:23:46 -0500

    Create Knight.java

---
+++ Knight.java
@@ -0,0 +1,20 @@
+public class Knight extends Piece {
+    public Knight(int x, int y, Side side, Board board) {
+        super(x,y,side,board);
+        // TODO: Call super constructor
+    }
+
+    @Override
+    public boolean canMove(int destX, int destY) {
+        if ((Math.abs(this.x - destX) == 2 && Math.abs(this.y  - destY) == 1) || (Math.abs(this.x - destX) == 1 && Math.abs(this.y  - destY) == 2)){
+            return true;
+        }
+        //TODO: Check piecerules.md for the movement rule for this piece :)
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return this.getSide() == Side.BLACK ? "♞" : "♘";
+    }
+}
