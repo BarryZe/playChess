4dcb269525c0b746f2e7393871fa76ba1ae9edab
BarryZe <98925337+BarryZe@users.noreply.github.com>
2023-01-14 14:24:31 -0500

    Create Rook.java

---
+++ Rook.java
@@ -0,0 +1,20 @@
+public class Rook extends Piece {
+    public Rook(int x, int y, Side side, Board board) {
+        super(x,y,side,board);
+        // TODO: Call super constructor
+    }
+
+    @Override
+    public boolean canMove(int destX, int destY) {
+        if(this.x == destX || this.y == destY){
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return this.getSide() == Side.BLACK ? "♜" : "♖";
+    }
+}
